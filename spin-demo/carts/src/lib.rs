27575f44fe64d51b49c2a40e6fe822121db7376b
Christian Speckner <christian.speckner@mayflower.de>
2024-06-20 15:55:11 +0200

    Routing.

--- spin-demo/carts/src/lib.rs
+++ spin-demo/carts/src/lib.rs
@@ -1,23 +1,108 @@
-use spin_sdk::http::{IntoResponse, Request, Response};
+use spin_sdk::http::{Request, Response};
 use spin_sdk::http_component;
+use url::Url;
 use urlpattern::{UrlPattern, UrlPatternInit};
 
-/// A simple Spin HTTP component.
 #[http_component]
-fn handle_carts(req: Request) -> anyhow::Result<impl IntoResponse> {
-    let patternDefCart = UrlPatternInit {
-        pathname: Some("/carts/:id".to_owned()),
-        ..Default::default()
-    };
+fn handle(req: Request) -> anyhow::Result<Response> {
+    let pattern_cart = build_pattern("/carts/:cartId");
+    let pattern_cart_items = build_pattern("/carts/:cartId/items");
+    let pattern_cart_item = build_pattern("/carts/:cartId/items/:itemId");
 
-    let patternCart = <UrlPattern>::parse(patternDefCart).unwrap();
+    let url = req
+        .header("spin-full-url")
+        .and_then(|u| u.as_str())
+        .and_then(|u| Url::parse(u).ok())
+        .unwrap();
 
-    let url = "https://example.com/users/123".parse().unwrap();
+    let match_cart = pattern_cart
+        .exec(urlpattern::UrlPatternMatchInput::Url(url.clone()))
+        .unwrap();
+    if match_cart.is_some() {
+        return handle_route_cart(
+            match_cart.unwrap().pathname.groups.get("cartId").unwrap(),
+            req,
+        );
+    }
 
-    println!("Handling request to {:?}", req.header("spin-full-url"));
+    let match_cart_items = pattern_cart_items
+        .exec(urlpattern::UrlPatternMatchInput::Url(url.clone()))
+        .unwrap();
+    if match_cart_items.is_some() {
+        return handle_route_cart_items(
+            match_cart_items
+                .as_ref()
+                .unwrap()
+                .pathname
+                .groups
+                .get("cartId")
+                .unwrap(),
+            req,
+        );
+    }
+
+    let match_cart_item = pattern_cart_item
+        .exec(urlpattern::UrlPatternMatchInput::Url(url.clone()))
+        .unwrap();
+    if match_cart_item.is_some() {
+        return handle_route_cart_item(
+            match_cart_item
+                .as_ref()
+                .unwrap()
+                .pathname
+                .groups
+                .get("cartId")
+                .unwrap(),
+            match_cart_item
+                .as_ref()
+                .unwrap()
+                .pathname
+                .groups
+                .get("itemId")
+                .unwrap(),
+            req,
+        );
+    }
+
+    Ok(Response::builder()
+        .status(404)
+        .header("content-type", "text/plain")
+        .body("not found")
+        .build())
+}
+
+fn handle_route_cart(cart_id: &str, req: Request) -> anyhow::Result<Response> {
     Ok(Response::builder()
         .status(200)
         .header("content-type", "text/plain")
-        .body("Hello, Fermyon --- /carts/...")
+        .body(format!("/carts/:id , id = {}", cart_id))
+        .build())
+}
+
+fn handle_route_cart_items(cart_id: &str, req: Request) -> anyhow::Result<Response> {
+    Ok(Response::builder()
+        .status(200)
+        .header("content-type", "text/plain")
+        .body(format!("/carts/:id/items , id = {}", cart_id))
+        .build())
+}
+
+fn build_pattern(source: &str) -> UrlPattern {
+    let pattern_init = UrlPatternInit {
+        pathname: Some(source.to_owned()),
+        ..Default::default()
+    };
+
+    <UrlPattern>::parse(pattern_init).unwrap()
+}
+
+fn handle_route_cart_item(cart_id: &str, item_id: &str, req: Request) -> anyhow::Result<Response> {
+    Ok(Response::builder()
+        .status(200)
+        .header("content-type", "text/plain")
+        .body(format!(
+            "/carts/:cartId/items/:itemId , cartId = {} , itemId = {}",
+            cart_id, item_id
+        ))
         .build())
 }
