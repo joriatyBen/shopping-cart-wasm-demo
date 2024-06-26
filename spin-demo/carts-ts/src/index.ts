efff54ec311911a3ccf1e046e20920151a0419d2
Christian Speckner <christian.speckner@mayflower.de>
2024-06-26 21:40:54 +0200

    Start implementing JS API. Phew.

--- spin-demo/carts-ts/src/index.ts
+++ spin-demo/carts-ts/src/index.ts
@@ -1,5 +1,40 @@
-import { HandleRequest, HttpRequest, HttpResponse } from '@fermyon/spin-sdk';
+import { HandleRequest, HttpRequest, HttpResponse, Pg } from '@fermyon/spin-sdk';
+import Ajv, { JTDDataType, Schema, ValidateFunction } from 'ajv/dist/jtd';
 import 'urlpattern-polyfill';
+
+type RdbmsParam = null | boolean | string | number | ArrayBuffer;
+
+interface QueryResult {
+    columns: Array<string>;
+    rows: Array<Array<RdbmsParam>>;
+}
+
+interface ParseBodyResultOK<T> {
+    ok: true;
+    body: T;
+}
+
+interface ParseBodyResultError {
+    ok: false;
+    error: string;
+}
+
+type ParseBodyResult<T> = ParseBodyResultOK<T> | ParseBodyResultError;
+
+const cartItemSchema = {
+    properties: {
+        id: { type: 'uint32' },
+        quantity: { type: 'uint32' },
+        price: { type: 'float64' },
+    },
+} as const;
+
+type CartItem = JTDDataType<typeof cartItemSchema>;
+
+const validators = new Map();
+const ajv = new Ajv();
+
+validators.set(cartItemSchema, ajv.compile<CartItem>(cartItemSchema));
 
 export const handleRequest: HandleRequest = async function (request: HttpRequest): Promise<HttpResponse> {
     const now = Date.now();
@@ -38,11 +73,7 @@ async function dispatch(request: HttpRequest): Promise<HttpResponse> {
         );
     }
 
-    return {
-        status: 200,
-        headers: { 'content-type': 'text/plain' },
-        body: `Hello from TS-SDK`,
-    };
+    return responseNotFound();
 }
 
 async function handleCarts(request: HttpRequest, cartId: number | undefined): Promise<HttpResponse> {
@@ -94,19 +125,35 @@ async function cartsGET(request: HttpRequest, cartId: number): Promise<HttpRespo
 }
 
 async function cartsItemsGET(request: HttpRequest, cartId: number): Promise<HttpResponse> {
-    return {
-        status: 200,
-        headers: { 'content-type': 'text/plain' },
-        body: `GET /carts/${cartId}/items`,
-    };
+    const queryResult = query('SELECT item_id, quantity, price FROM cart.cart_items WHERE cart_id = $1', [cartId]);
+
+    if (queryResult.rows.length === 0) return responseNotFound();
+
+    const result: Array<CartItem> = queryResult.rows.map(([id, quantity, price]) => ({
+        id: id as number,
+        quantity: quantity as number,
+        price: price as number,
+    }));
+
+    return responseJson(result);
 }
 
 async function cartsItemsPOST(request: HttpRequest, cartId: number): Promise<HttpResponse> {
-    return {
-        status: 200,
-        headers: { 'content-type': 'text/plain' },
-        body: `POST /carts/${cartId}/items`,
-    };
+    const parsedBody = parseBody(request, cartItemSchema);
+    if (!parsedBody.ok) {
+        return responseBadRequest(parsedBody.error);
+    }
+
+    const item = parsedBody.body;
+    item.price += Number.EPSILON;
+
+    try {
+        execute('INSERT INTO cart.cart_items VALUES($1, $2, $3, $4)', [cartId, item.id, item.quantity, item.price]);
+    } catch (e) {
+        return responseBadRequest('duplicate key');
+    }
+
+    return responseJson(item);
 }
 
 async function cartsItemsPATCH(request: HttpRequest, cartId: number): Promise<HttpResponse> {
@@ -140,9 +187,69 @@ function responseBadRequest(message: string): HttpResponse {
     };
 }
 
+function responseInternalServerError(message: string): HttpResponse {
+    return {
+        status: 500,
+        body: message,
+    };
+}
+
+function responseJson<T>(payload: T): HttpResponse {
+    return {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload),
+    };
+}
+
+function responseNotFound(): HttpResponse {
+    return {
+        status: 404,
+    };
+}
+
 function parseId(serializedId: string | undefined): number | undefined {
     if (serializedId === undefined) return undefined;
     if (!/^\d+$/.test(serializedId)) return undefined;
 
     return parseInt(serializedId, 10);
+}
+
+function dbUrl(): string {
+    const url = process.env['DB_URL'];
+
+    if (!url) throw new Error('DB_URL not configured');
+
+    return url;
+}
+
+function query(sql: string, params: Array<RdbmsParam>): QueryResult {
+    return Pg.query(dbUrl(), sql, params);
+}
+
+function execute(sql: string, params: Array<RdbmsParam>): void {
+    return Pg.execute(dbUrl(), sql, params);
+}
+
+function validator<T extends Schema>(schema: T): ValidateFunction<JTDDataType<T>> {
+    return validators.get(schema);
+}
+
+function parseBody<T extends Schema>(request: HttpRequest, schema: T): ParseBodyResult<JTDDataType<T>> {
+    if (!request.body) return { ok: false, error: 'missing body' };
+
+    try {
+        const decoder = new TextDecoder();
+        const body = JSON.parse(decoder.decode(request.body));
+        const validate = validator(schema);
+
+        if (!validate(body)) {
+            return { ok: false, error: ajv.errorsText(validate.errors) };
+        }
+
+        return { ok: true, body };
+    } catch (e) {
+        console.log(e);
+        return { ok: false, error: e + '' };
+    }
 }
