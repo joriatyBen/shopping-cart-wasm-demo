df594dfe1db12868c779b275414bf0dde4f67814
joriatyBen <paul.bajorat@gmail.com>
2024-07-19 16:28:15 +0200

    fix tf azure backend

--- terraform/wasmdemo/providers.tf
+++ terraform/wasmdemo/providers.tf
@@ -10,12 +10,12 @@ terraform {
       version = "~>3.6.0"
     }
   }
-#   backend "azurerm" {
-#     resource_group_name  = "wasmdemo"
-#     storage_account_name = "wasmdemo"
-#     container_name       = "wasmdemo"
-#     key                  = "terraform.tfstate"
-#   }
+  backend "azurerm" {
+    resource_group_name  = "wasmdemo"
+    storage_account_name = "wasmdemo"
+    container_name       = "wasmdemo"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
